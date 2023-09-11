beda742cc61c41337ccebdcf601b8d35e6be77e6
Emin-Tufan <93204330+Emin-Tufan@users.noreply.github.com>
2023-09-11 12:17:49 +0300

    Delete Frontend directory

--- Frontend/src/components/adminPages/PrivateAdminRouter.js
+++
@@ -1,8 +0,0 @@
-import {Navigate} from "react-router-dom";
-
-export default function PrivateAdminRouter({children,isAdminLogin}){
-    if (!isAdminLogin) {
-      return <Navigate to="/admin" />
-    }
-    return children
-}
